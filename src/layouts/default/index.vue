f4c2eb732ddaee3fff381abec67714d36fa36f8d
unknown <vuhuyquang2k@gmail.com>
2024-06-22 01:41:25 +0700

    update code

--- src/layouts/default/index.vue
+++ src/layouts/default/index.vue
@@ -1,9 +1,9 @@
 <template>
   <a-layout class="h-[100vh]">
     <Header />
-      <a-layout-content class="container mx-auto w-screen max-w-full pt-[76px]">
-        <router-view></router-view>
-      </a-layout-content>
+    <a-layout-content class="container mx-auto w-screen max-w-full pt-[76px]">
+      <router-view></router-view>
+    </a-layout-content>
     <Footer />
   </a-layout>
 </template>
