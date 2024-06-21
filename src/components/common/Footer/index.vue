9468ef2a8cb8d2e26bbf4b5b0272e095328379e1
unknown <vuhuyquang2k@gmail.com>
2024-06-21 23:44:53 +0700

    update code

--- src/components/common/Footer/index.vue
+++ src/components/common/Footer/index.vue
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <template>
   <div class="grid grid-rows-2 gap-[80px] bg-black text-white pt-[8rem] px-[100px] px-custom font-custom">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
@@ -42,49 +41,4 @@
 
 <script lang="ts" setup></script>
 
-=======
-<template>
-  <div class="flex flex-col gap-[80px] bg-black mt-[5rem] h-screen text-white pt-[8rem] px-[100px] px-custom font-custom">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-      <div class="flex flex-col gap-5">
-        <h2>GIỚI THIỆU</h2>
-        <p class="text-gray-300">Là lập trình viên website app, blogger và là Founder của Quag.</p>
-      </div>
-      <div class="flex flex-col gap-5">
-        <h2>THÔNG TIN</h2>
-        <ul class="text-gray-300 space-y-4">
-          <li>Chính sách</li>
-          <li>Điều khoản</li>
-        </ul>
-      </div>
-      <div class="flex flex-col gap-5">
-        <h2>ĐƯỜNG DẪN</h2>
-        <ul class="text-gray-300 space-y-4">
-          <li>
-            <NuxtLink to="/">Trang chủ</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/bai-viet">Bài viết</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="/lien-he">Liên hệ</NuxtLink>
-          </li>
-        </ul>
-      </div>
-      <div class="flex flex-col gap-5">
-        <h2>BẠN CÓ CÂU HỎI?</h2>
-        <ul class="text-gray-300 space-y-4">
-          <li>Thanh Xuân, Hà Nội</li>
-          <li>+84 34 439 6798</li>
-          <li>quangvh.technical@gmail.com</li>
-        </ul>
-      </div>
-    </div>
-    <div class="flex justify-center text-gray-400">Copyright © 2023 All rights reserved | Made by Quang Vu Huy</div>
-  </div>
-</template>
-
-<script lang="ts" setup></script>
-
->>>>>>> 1caa8d75a119d3e03c0decc5aab3a21bd403c254
 <style scoped src="./styles.scss"></style>
